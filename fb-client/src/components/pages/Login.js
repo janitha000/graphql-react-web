7ad51787c83f8e9bf4205637acb0e2f9dd3110d4
Janitha Tennakoon <janitha000@gmail.com>
2021-02-04 16:04:16 +0800

    Add login and register logic

--- fb-client/src/components/pages/Login.js
+++ fb-client/src/components/pages/Login.js
@@ -1,12 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
+import { Link } from 'react-router-dom';
+import Loader from "react-loader-spinner";
 
-const Login = () => {
+import './Register.css'
+import { useForm } from '../../hooks/useForm';
+
+const Login = ({ history }) => {
+
+    const [result, setResult] = useState({})
+    const [errors, setErrors] = useState('')
+
+    const { onChange, onSubmit, clearForm, values } = useForm(login, {
+        username: '',
+        password: ''
+    })
+
+    const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+        update(_, result) {
+            console.log(result)
+            let { data: { login } } = result
+            setResult(login)
+            history.push('/')
+        },
+        onError(err) {
+            console.log(err.toString())
+            setErrors(err.toString())
+        },
+        variables: values
+    })
+
+    function login() {
+        loginUser()
+        clearForm();
+    }
+
     return (
-        <div>
-            This is login page
+        <div className="register">
+            <div className="login-box">
+                <h2>Login</h2>
+                <form>
+                    <div className="user-box">
+                        <input type="text" name="username" value={values.username} onChange={(e) => onChange(e)} required />
+                        <label>Username</label>
+                    </div>
+
+                    <div className="user-box">
+                        <input type="password" name="password" value={values.password} onChange={(e) => onChange(e)} required />
+                        <label>Password</label>
+                    </div>
+                    <a href="#" onClick={onSubmit}>  Submit</a>
+
+                </form>
+                <div className="register-message">
+                    {loading && (<Loader type="Bars" color="black" height={50} width={50} />)}
+                    {!loading && errors && (<p style={{ color: 'red' }}>{errors}</p>)}
+
+                </div>
+            </div>
 
         </div>
     )
 }
+
+const LOGIN_USER = gql`
+    mutation login(
+        $username: String!
+        $password: String!
+    ) {
+        login(
+            loginInput: {
+                username: $username
+                password: $password
+            }
+        ) {
+            user { id, username, email, createdAt}
+            token
+        }
+    }
+    
+`
 
 export default Login
--- fb-client/src/components/pages/Register.js
+++ fb-client/src/components/pages/Register.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
+import { Link } from 'react-router-dom';
+import Loader from "react-loader-spinner";
+
+import './Register.css'
+import { useForm } from '../../hooks/useForm';
 
 const Register = () => {
+
+    const [result, setResult] = useState({})
+    const [errors, setErrors] = useState('')
+    const { onChange, onSubmit, clearForm, values } = useForm(registerUser, { username: '', email: '', password: '' })
+
+    const [addUser, { loading }] = useMutation(REGISTER_USER, {
+        update(_, result) {
+            console.log(result)
+            let { data: { register } } = result
+            setResult(register)
+        },
+        onError(err) {
+            console.log(err.toString())
+            setErrors(err.toString())
+        },
+        variables: values
+    })
+
+    function registerUser() {
+        addUser();
+        clearForm({ username: '', email: '', password: '' });
+    }
+
     return (
-        <div>
-            This is register page
+        <div className="register">
+            <div className="login-box">
+                <h2>Register</h2>
+                <form>
+                    <div className="user-box">
+                        <input type="text" name="username" value={values.username} onChange={(e) => onChange(e)} required />
+                        <label>Username</label>
+                    </div>
+                    <div className="user-box">
+                        <input type="text" name="email" value={values.email} onChange={(e) => onChange(e)} required />
+                        <label>Email</label>
+                    </div>
+                    <div className="user-box">
+                        <input type="password" name="password" value={values.password} onChange={(e) => onChange(e)} required />
+                        <label>Password</label>
+                    </div>
+                    <a href="#" onClick={onSubmit}>  Submit</a>
+
+                </form>
+                <div className="register-message">
+                    {loading && (<Loader type="Bars" color="black" height={50} width={50} />)}
+                    {!loading && result && result.user && (<p>User Registered Successfully. Navigate to <Link to="/login" >Login</Link> page.</p>)}
+                    {!loading && errors && (<p style={{ color: 'red' }}>{errors}</p>)}
+
+                </div>
+            </div>
+
         </div>
     )
 }
+
+const REGISTER_USER = gql`
+    mutation register(
+        $username: String!
+        $password: String!
+        $email: String!
+    ) {
+        register(
+            registerInput: {
+                username: $username
+                password: $password
+                email: $email
+            }
+        ) {
+            user { id, username, email, createdAt}
+            token
+        }
+    }
+    
+`
 
 export default Register
